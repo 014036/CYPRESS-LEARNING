de686fbb8e7fcd983e0d9cf341c2d89842563e7c
014036 <kusulu78@gmail.com>
2023-11-11 02:06:01 +0300

    10Kasim2023

--- cypress/e2e/assertion.cy.js
+++ cypress/e2e/assertion.cy.js
@@ -13,11 +13,33 @@ it('Assertion Testi', ()=>{
     .and('contain', 'koa')
     .and('eq', 'https://www.koalaresorthotels.com/')
 
+
+    //ASSERTION YAPMA
     cy.get('a[class="navbar-brand"]').should('be.visible')
+    //VEYA
     cy.get('a[class="navbar-brand"]').should('exist')
+    //VEYA
+    cy.get('a[class="navbar-brand"]').should('have value', '')
 
   
 
+})
+
+it('Explicit Assertion', function(){
+
+    cy.visit('https://www.koalaresorthotels.com/')
+    cy.get('#navLogon').click()
+    cy.get('[placeholder="Username"]').type('emre')
+    cy.get('[placeholder="Password"]').type('EmrE12345.')
+    cy.get('#btnSubmit').click()
+
+    let expName='emre'
+
+    cy.get('#edit-form > :nth-child(2)').then(function(x){
+        let actualName=x.text()
+        
+
+    })
 })
 
 })
