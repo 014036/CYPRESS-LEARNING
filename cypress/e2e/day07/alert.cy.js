4df6c827344643922280a9d1365694fc68d53a7f
014036 <kusulu78@gmail.com>
2023-11-12 16:32:13 +0300

    12Kasim2023-1

--- cypress/e2e/day07/alert.cy.js
+++ cypress/e2e/day07/alert.cy.js
@@ -28,16 +28,41 @@ describe('Alert testleri',function(){
     })
 
     //CONFIRM ALERT HANDLE ETMEK 'CANCEL' VE 'OK' OLAN ALERTLER ICIN
-    it.only('Java Script Alert Test01',()=>{
+    it('Java Script Alert Test01',()=>{
         cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
 
         cy.get('[onclick="jsConfirm()"]').click();
         cy.on('window:confirm', function(t){
             expect(t).to.contain('I am a JS Confirm');
         });
+
         cy.on('window:confirm',(t)=>false);
+       
         cy.get('#result').should('have.text','You clicked: Cancel')
   
 
     })
+
+
+    //ALERTLERE TEXT GONDERIP OK BUTONUNA BASMAK ICIN
+
+    it('Promp Alert Test01',()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
+        
+        cy.window().then((win)=>{
+            cy.stub(win,'prompt').returns('Merhaba')
+        });
+        cy.get('[onclick="jsPrompt()"]').click();
+        cy.get('#result').should('have.text', 'You entered: Merhaba')
+  
+
+    })
+
+
+//AUTHENTICATION ALERT
+it.only('Authentication Alert Test 01',function(){
+    cy.visit('https://the-internet.herokuapp.com/basic_auth');
+})
+
+     
 })
